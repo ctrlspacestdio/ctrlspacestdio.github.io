e81a5c6d0df18b86303d3c296f416af399764aa8
Cristian wibowo <bowok25@gmail.com>
2024-11-27 01:51:46 +0700

    update 1

--- js/active.js
+++ js/active.js
@@ -67,5 +67,64 @@
         $(this).addClass('active');
     })
 
+    // :: 4.0 ScrollUp Active JS
+    if ($.fn.scrollUp) {
+        $.scrollUp({
+            scrollSpeed: 1500,
+            scrollText: '<i class="fa fa-arrow-up" aria-hidden="true"></i>'
+        });
+    }
 
+    // :: 5.0 CounterUp Active JS
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
+
+    // :: 6.0 onePageNav Active Code
+    if ($.fn.onePageNav) {
+        $('#fplusNav').onePageNav({
+            currentClass: 'active',
+            scrollSpeed: 2000,
+            easing: 'easeOutQuad'
+        });
+    }
+
+    // :: 7.0 Video Active Code
+    if ($.fn.magnificPopup) {
+        $('.video--play--btn').magnificPopup({
+            disableOn: 0,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 10,
+            preloader: true,
+            fixedContentPos: false
+        });
+    }
+
+    // :: 6.0 PreventDefault a Click
+    $("a[href='#']").on('click', function ($) {
+        $.preventDefault();
+    });
+
+    // :: 9.0 wow Active Code
+    if ($.fn.init) {
+        new WOW().init();
+    }
+
+    // :: 10.0 matchHeight Active JS
+    if ($.fn.matchHeight) {
+        $('.item').matchHeight();
+    }
+
+    var $window = $(window);
+
+    // :: 11.0 Preloader active code
+    $window.on('load', function () {
+        $(document).ready(function () {
+            $("#preloader").fadeOut();
+        })
+    });
 })(jQuery);
